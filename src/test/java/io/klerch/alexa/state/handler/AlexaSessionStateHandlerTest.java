8ebef811b01d97fab545370b66896696f9ca8500
Lerch, Kay <kay.lerch@immobilienscout24.de>
2016-08-30 21:03:35 +0200

    Add author-headers

--- src/test/java/io/klerch/alexa/state/handler/AlexaSessionStateHandlerTest.java
+++ src/test/java/io/klerch/alexa/state/handler/AlexaSessionStateHandlerTest.java
@@ -1,3 +1,9 @@
+/**
+ * Made by Kay Lerch (https://twitter.com/KayLerch)
+ *
+ * Attached license applies.
+ * This library is licensed under GNU GENERAL PUBLIC LICENSE Version 3 as of 29 June 2007
+ */
 package io.klerch.alexa.state.handler;
 
 public class AlexaSessionStateHandlerTest extends AlexaStateHandlerTest<AlexaSessionStateHandler> {
--- src/test/java/io/klerch/alexa/state/model/Model.java
+++ src/test/java/io/klerch/alexa/state/model/Model.java
@@ -1,3 +1,9 @@
+/**
+ * Made by Kay Lerch (https://twitter.com/KayLerch)
+ *
+ * Attached license applies.
+ * This library is licensed under GNU GENERAL PUBLIC LICENSE Version 3 as of 29 June 2007
+ */
 package io.klerch.alexa.state.model;
 
 import java.util.ArrayList;
